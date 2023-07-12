53348d941c522f30261808be213eb22408d421b2
Andrew Shields <alsh227@g.uky.edu>
2023-07-12 13:11:12 -0400

    Reorder levels. Format client version differently (e.g. 0.2 -> 0.20)

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -42,8 +42,14 @@ export const Table: React.FunctionComponent<ITableProps> = (props: ITableProps) 
 					headerName: key
 				}
 
-				if (key === "ApiVersion" || key === "ClientVersion") {
+				if (key === "ApiVersion") {
 					columnDef.type = "number";
+				} else if (key === "ClientVersion") {
+					columnDef.type = "number";
+					columnDef.valueFormatter = (params) => {
+						// 0.2 -> 0.20
+						return `${params.value.toString().padEnd(4, "0")}`;
+					};
 				} else if (key === "RowKey") {
 					columnDef.width = 500;
 				} else if (key.startsWith("Level")) {
@@ -102,46 +108,48 @@ export const Table: React.FunctionComponent<ITableProps> = (props: ITableProps) 
 			const mapLevelKeyToOrderIndex: any = {
 				Level13: 0, // DEMO_HIT_IT
 				Level0: 1, // Original
+				Level35: 2, // Baby volcano
+				Level36: 3, // Gundam
 
-				Level15: 2, // DEMO_FREE_IT
-				Level1: 3, // Zig zag
-				Level20: 4, // Triangles
-				Level2: 5, // Up right, launch left
-				Level16: 6, // Volcano
-				Level17: 7, // Max launch
-				Level19: 8, // Slow layer drop
+				Level15: 4, // DEMO_FREE_IT
+				Level1: 5, // Zig zag
+				Level20: 6, // Triangles
+				Level16: 7, // Volcano
+				Level17: 8, // MaLevel launch
+				Level2: 9, // Up right, launch left
 
-				Level22: 9, // DEMO_CHIP_IT
-				Level32: 10, // Chip it, spider
-				Level3: 11, // Chip it
-				Level6: 12, // Semicircle
+				Level22: 10, // DEMO_CHIP_IT
+				Level32: 11, // Chip it, spider
+				Level3: 12, // Chip it
 				Level4: 13, // Chip it, ledges
 				Level7: 14, // Spiral
 
 				Level23: 15, // DEMO_ROLL_IT
-				Level28: 16, // Roll it, chip it, triangles
-				Level26: 17, // Roll it, zig zag
-				Level25: 18, // Cross
-				Level34: 19, // Roll it, maze
-				Level5: 20, // Roll it
+				Level26: 16, // Roll it, zig zag
+				Level6: 17, // Semicircle
+				Level28: 18, // Roll it, chip it, triangles
+				Level25: 19, // Cross
+				Level34: 20, // Roll it, maze
 				Level18: 21, // Slide sniper
+				Level5: 22, // Roll it
 
-				Level24: 22, // DEMO_JUGGLE_IT
-				Level8: 23, // Juggle it
-				Level29: 24, // Cross octagon
-				Level33: 25, // V
-				Level9: 26, // Juggle it, island
-				Level21: 27, // Slow drop sniper
+				Level24: 23, // DEMO_JUGGLE_IT
+				Level19: 24, // Slow layer drop
+				Level8: 25, // Juggle it
+				Level29: 26, // Cross octagon
+				Level33: 27, // V
+				Level9: 28, // Juggle it, island
+				Level21: 29, // Slow drop sniper
 
-				Level14: 28, // DEMO_LOAD_IT
-				Level30: 29, // Load and skip it
-				Level31: 30, // Load it, roll it
-				Level11: 31, // Stairs
-				Level10: 32, // Load it
-				Level12: 33, // Finale
+				Level14: 30, // DEMO_LOAD_IT
+				Level30: 31, // Load and skip it
+				Level31: 32, // Load it, roll it
+				Level11: 33, // Stairs
+				Level10: 34, // Load it
+				Level12: 35, // Finale
 
 				// Dead levels
-				Level27: 34, // Minecraft ///////////////////// DEAD
+				//Level27, // Minecraft ///////////////////// DEAD
 			};
 
 			// Must offset by known columns before this one
@@ -169,7 +177,7 @@ export const Table: React.FunctionComponent<ITableProps> = (props: ITableProps) 
 			});
 		} else {
 			// Debug
-			const DEBUG_DATA = [{"ApiVersion":1,"ClientVersion":0.16,"PartitionKey":"World0","RowKey":"Mac_Andrew’s MacBook Air_84C0D352-53D7-5948-8E18-985A0FB59F7E","Level0":1.23,"Level1":12.34,"Level2":1234.56,"Level3":-1.0,"Level4":-1.0,"Level5":-1.0,"Level6":-1.0,"Level7":-1.0,"Level8":-1.0,"Level9":-1.0,"Level10":-1.0,"Level11":-1.0,"Level12":-1.0,"Level13":2.51,"Level14":-1.0,"Level15":6.7,"Level16":1800,"Level17":-1.0,"Level18":-1.0,"Level19":-1.0,"Level20":123.45,"Level21":-1.0,"Level22":-1.0,"Level23":-1.0,"Level24":-1.0,"Level25":-1.0,"Level26":-1.0,"Level27":-1.0,"Level28":-1.0,"Level29":-1.0,"Level30":-1.0,"Level31":-1.0,"Level32":-1.0,"Level33":-1.0,"Level34":-1.0,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-04-28T03:03:20.9401379+00:00","ETag":{}},{"ApiVersion":1,"ClientVersion":0.15,"PartitionKey":"World0","RowKey":"PC_ANDREW-ASUS_14cc113631afa34b6aa11947cbaa019d2375fdf1","Level0":9.21,"Level1":-1.0,"Level2":-1.0,"Level3":-1.0,"Level4":-1.0,"Level5":-1.0,"Level6":-1.0,"Level7":-1.0,"Level8":-1.0,"Level9":-1.0,"Level10":-1.0,"Level11":-1.0,"Level12":-1.0,"Level13":2.33,"Level14":-1.0,"Level15":3.85,"Level16":-1.0,"Level17":-1.0,"Level18":-1.0,"Level19":-1.0,"Level20":-1.0,"Level21":-1.0,"Level22":-1.0,"Level23":-1.0,"Level24":-1.0,"Level25":-1.0,"Level26":-1.0,"Level27":-1.0,"Level28":-1.0,"Level29":-1.0,"Level30":-1.0,"Level31":-1.0,"Level32":-1.0,"Level33":-1.0,"Level34":-1.0,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-04-28T02:51:01.1204041+00:00","ETag":{}},{"ApiVersion":1,"ClientVersion":0.16,"PartitionKey":"World0","RowKey":"Shields_iPhone_70A19FC6-1265-44DD-9432-C46C7FC79C6E","Level0":8.32,"Level1":20.19,"Level2":11.7,"Level3":-1.0,"Level4":-1.0,"Level5":-1.0,"Level6":-1.0,"Level7":-1.0,"Level8":-1.0,"Level9":-1.0,"Level10":-1.0,"Level11":-1.0,"Level12":-1.0,"Level13":3.16,"Level14":-1.0,"Level15":3.95,"Level16":10.07,"Level17":6.53,"Level18":-1.0,"Level19":43.3,"Level20":18.87,"Level21":-1.0,"Level22":7.11,"Level23":-1.0,"Level24":-1.0,"Level25":-1.0,"Level26":-1.0,"Level27":-1.0,"Level28":-1.0,"Level29":-1.0,"Level30":-1.0,"Level31":-1.0,"Level32":-1.0,"Level33":-1.0,"Level34":-1.0,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-04-28T03:53:58.754059+00:00","ETag":{}}];
+			const DEBUG_DATA = [{"ApiVersion":1,"ClientVersion":0.20,"PartitionKey":"World0","RowKey":"4th_iPhone_70A19FC6-1265-44DD-9432-C46C7FC79C6E","Level0":1800.0,"Level1":1800.0,"Level2":1800.0,"Level3":241.73,"Level4":53.86,"Level5":1800.0,"Level6":923.36,"Level7":1800.0,"Level8":1800.0,"Level9":1800.0,"Level10":1800.0,"Level11":1800.0,"Level12":1800.0,"Level13":18.13,"Level14":1800.0,"Level15":1800.0,"Level16":1800.0,"Level17":1800.0,"Level18":1800.0,"Level19":1800.0,"Level20":1800.0,"Level21":1800.0,"Level22":20.66,"Level23":1800.0,"Level24":1800.0,"Level25":1800.0,"Level26":1800.0,"Level27":-1.0,"Level28":1800.0,"Level29":1800.0,"Level30":1800.0,"Level31":1800.0,"Level32":41.41,"Level33":1800.0,"Level34":1800.0,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-07-02T18:08:50.3077486+00:00","ETag":{}},{"ApiVersion":1,"ClientVersion":0.19,"PartitionKey":"World0","RowKey":"Bal_iPhone_70A19FC6-1265-44DD-9432-C46C7FC79C6E","Level0":14.65,"Level1":30.8,"Level2":27.73,"Level3":-1.0,"Level4":-1.0,"Level5":-1.0,"Level6":-1.0,"Level7":-1.0,"Level8":-1.0,"Level9":-1.0,"Level10":-1.0,"Level11":-1.0,"Level12":-1.0,"Level13":5.22,"Level14":-1.0,"Level15":32.74,"Level16":47.63,"Level17":80.72,"Level18":-1.0,"Level19":-1.0,"Level20":46.46,"Level21":-1.0,"Level22":-1.0,"Level23":-1.0,"Level24":-1.0,"Level25":-1.0,"Level26":-1.0,"Level27":-1.0,"Level28":-1.0,"Level29":-1.0,"Level30":-1.0,"Level31":-1.0,"Level32":-1.0,"Level33":-1.0,"Level34":-1.0,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-07-02T17:37:19.6239599+00:00","ETag":{}},{"ApiVersion":1,"ClientVersion":0.19,"PartitionKey":"World0","RowKey":"First 0.19_iPhone_70A19FC6-1265-44DD-9432-C46C7FC79C6E","Level0":7.84,"Level1":23.09,"Level2":21.83,"Level3":35.65,"Level4":55.7,"Level5":86.89,"Level6":84.25,"Level7":152.28,"Level8":95.31,"Level9":273.81,"Level10":370.54,"Level11":96.39,"Level12":202.64,"Level13":2.81,"Level14":10.25,"Level15":11.8,"Level16":25.3,"Level17":58.48,"Level18":50.4,"Level19":35.14,"Level20":34.97,"Level21":74.43,"Level22":5.51,"Level23":8.54,"Level24":16.65,"Level25":51.79,"Level26":62.72,"Level27":-1.0,"Level28":46.2,"Level29":46.5,"Level30":221.17,"Level31":149.92,"Level32":38.87,"Level33":65.17,"Level34":111.91,"Level35":-1.0,"Level36":-1.0,"Level37":-1.0,"Level38":-1.0,"Level39":-1.0,"Level40":-1.0,"Level41":-1.0,"Level42":-1.0,"Level43":-1.0,"Level44":-1.0,"Level45":-1.0,"Level46":-1.0,"Level47":-1.0,"Level48":-1.0,"Level49":-1.0,"Level50":-1.0,"Timestamp":"2023-06-30T16:53:12.0987106+00:00","ETag":{}}];
 			populateTable(DEBUG_DATA);
 		}
 	}, []);
